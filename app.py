ce510c16b65c53410661ff0285818fca8a3282d6
nest23081 <41284205+nest23081@users.noreply.github.com>
2021-04-27 11:11:13 +0900

    rest is awsome

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 import json
 
 app=Flask(__name__)
 
-@app.route('/rest')
-def home():
+@app.route('/')
+def index():
     res = requests.get('http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=GdWTDEeLjnO0YeiW5E4occlcHvqg03IlkDh5FCkW/JpS4g1l2idd09uocu8pLVYvNBp3GL0qvikxil85BaJy4A==&numOfRows=10&pageNo=1&base_date=20210427&base_time=0230&nx=55&ny=127&dataType=JSON')
     return res.text
 
