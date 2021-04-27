260b1f0907cc7497a67ecb79749c7703baa3ce67
nest23081 <41284205+nest23081@users.noreply.github.com>
2021-04-27 10:36:50 +0900

    rest2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('a.html')
+    return render_template('index.html')
 @app.route('/rest')
 def home():
     res = requests.get('http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=GdWTDEeLjnO0YeiW5E4occlcHvqg03IlkDh5FCkW/JpS4g1l2idd09uocu8pLVYvNBp3GL0qvikxil85BaJy4A==&numOfRows=10&pageNo=1&base_date=20210427&base_time=0230&nx=55&ny=127&dataType=JSON')
